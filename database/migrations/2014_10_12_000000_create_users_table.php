91145fc2e400772767876858eae2cd62d5cc8aef
Guillaume <guillaume.briard@gmail.com>
2019-04-19 14:44:20 +0700

    edit migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,7 +24,7 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
 
-            $table->foreign('customer_id')->references('id')->on('customers');
+            //$table->foreign('customer_id')->references('id')->on('customers');
         });
     }
 
