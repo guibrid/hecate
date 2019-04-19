92975e12b27a1543bdd935020884fc5208e74853
Guillaume <guillaume.briard@gmail.com>
2019-04-19 14:41:58 +0700

    edit migration file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,14 +20,10 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('tel', 50)->nullable();
-            $table->unsignedBigInteger('customer_id')->nullable();
+            $table->BigInteger('customer_id')->unsigned()->nullable();
             $table->rememberToken();
             $table->timestamps();
 
-            
-        });
-
-        Schema::table('users', function($table) {
             $table->foreign('customer_id')->references('id')->on('customers');
         });
     }
