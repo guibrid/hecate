3dea538d533f832982a3b770da6562ec3754e6a9
Guillaume <guillaume.briard@gmail.com>
2019-07-25 15:48:18 +0200

    add Destination columns in order table

--- resources/views/admin/orders/inc/ordersTable.blade.php
+++ resources/views/admin/orders/inc/ordersTable.blade.php
@@ -2,41 +2,54 @@
         <thead>
           <tr>
             <th>Customer</th>
-            <th>Consignee</th>
             <th>Shipper/Supplier</th>
             <th>Booking n°</th>
             <th>B/L & HAWB n°</th>
+            <th>Order number</th>
             <th>Status</th>
             <th>Shipment</th>
+            <th>Destination</th>
             <th>Shipment Mode</th>
             <th># packages</th>
+            <th>Consignee</th>
             <th>Weight</th>
             <th>Volume</th>
+            <th>Value</th>
             <th>Action</th>
           </tr>
         </thead>
         <tbody>
           @foreach ($orders as $order)
           <tr>
-                  <td>{{$order->customer->name}}</td>
-                  <td>{{$order->recipient}}</td>
-                  <td>{{$order->supplier}}</td>
-                  <td>{{$order->number}}</td>
-                  <td>{{$order->bl_number }}</td>
-                  <td>{{$order->status->title}}</td>
-                  <td>@php echo Helpers::renderShipmentStatus($order->shipment); @endphp</td>
-                  <td>{{strtoupper($order->load)}}</td>
-                  <td>{{$order->package_number}}</td>
-                  <td>@if ($order->weight) {{$order->weight}} kg @endif</td>
-                  <td>@if ($order->volume) {{$order->volume}} m3 @endif</td>
-                  <td>
-                    {{ Form::open(['method'=>'DELETE', 'route'=>['order.delete', $order->id]]) }}
-                    <a href="{{ URL::route('order.edit', $order->id) }}" type="button" class="btn btn-primary btn-xs">edit</a> 
-                    <input class="btn btn-danger btn-xs" onclick="return confirm('Are you sure you want to delete this order?');" type="submit" value="Del"> 
-                    {{ Form::close() }}
-                  </td>
+              <td>{{$order->customer->name}}</td>
+              <td>{{$order->supplier}}</td>
+              <td>{{$order->number}}</td>
+              <td>{{$order->bl_number }}</td>
+              <td>{{$order->order_number }}</td>
+              <td>{{$order->status['title']}}</td>
+              <td>@php echo Helpers::renderShipmentStatus($order->shipment); @endphp</td>
+              <td>
+                  @if ($order->shipment && $order->shipment->transshipments)
+                    @php $lastTransshipment = end($order->shipment->transshipments) @endphp
+                    @if(isset($lastTransshipment[0]))
+                      {{$lastTransshipment[0]->destination->title}}
+                    @endif
+                  @endif
+              </td>
+              <td>{{strtoupper($order->load)}}</td>
+              <td>{{$order->package_number}}</td>
+              <td>{{$order->recipient}}</td>
+              <td>@if ($order->weight) {{$order->weight}} kg @endif</td>
+              <td>@if ($order->volume) {{$order->volume}} m3 @endif</td>
+              <td>{{$order->value}}</td>
+              <td>
+                {{ Form::open(['method'=>'DELETE', 'route'=>['order.delete', $order->id]]) }}
+                <a href="{{ URL::route('order.edit', $order->id) }}" type="button" class="btn btn-primary btn-xs">edit</a> 
+                <input class="btn btn-danger btn-xs" onclick="return confirm('Are you sure you want to delete this order?');" type="submit" value="Del"> 
+                {{ Form::close() }}
+              </td>
 
-                </tr>
+            </tr>
           @endforeach
 
         </tbody>
--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -13,58 +13,10 @@
         <div class="clearfix"></div>
       </div>
       <div class="x_content">
-
-        <table id="datatable-responsive" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
-          <thead>
-            <tr>
-              <th>Customer</th>
-              <th>Consignee</th>
-              <th>Shipper/Supplier</th>
-              <th>Booking n°</th>
-              <th>B/L & HAWB n°</th>
-              <th>Order number</th>
-              <th>Status</th>
-              <th>Shipment</th>
-              <th>Shipment Mode</th>
-              <th># packages</th>
-              <th>Weight</th>
-              <th>Volume</th>
-              <th>Value</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            @foreach ($orders as $order)
-            <tr>
-                    <td>{{$order->customer->name}}</td>
-                    <td>{{$order->recipient}}</td>
-                    <td>{{$order->supplier}}</td>
-                    <td>{{$order->number}}</td>
-                    <td>{{$order->bl_number }}</td>
-                    <td>{{$order->order_number }}</td>
-                    <td>{{$order->status['title']}}</td>
-                    <td>@php echo Helpers::renderShipmentStatus($order->shipment); @endphp</td>
-                    <td>{{strtoupper($order->load)}}</td>
-                    <td>{{$order->package_number}}</td>
-                    <td>@if ($order->weight) {{$order->weight}} kg @endif</td>
-                    <td>@if ($order->volume) {{$order->volume}} m3 @endif</td>
-                    <td>{{$order->value}}</td>
-                    <td>
-                      {{ Form::open(['method'=>'DELETE', 'route'=>['order.delete', $order->id]]) }}
-                      <a href="{{ URL::route('order.edit', $order->id) }}" type="button" class="btn btn-primary btn-xs">edit</a> 
-                      <input class="btn btn-danger btn-xs" onclick="return confirm('Are you sure you want to delete this order?');" type="submit" value="Del"> 
-                      {{ Form::close() }}
-                    </td>
-
-                  </tr>
-            @endforeach
-
-          </tbody>
-        </table>
-        
+          @include('admin.orders.inc.ordersTable', ['orders' => $orders])
       </div>
     </div>
-  </div>
+</div>
 
 @endsection
 
@@ -73,40 +25,24 @@
     <!-- Datatables -->
     <link href="{{ asset('bower_components/gentelella/vendors/datatables.net-bs/css/dataTables.bootstrap.min.css')}}" rel="stylesheet">
     <link href="{{ asset('bower_components/gentelella/vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{ asset('bower_components/gentelella/vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css')}}" rel="stylesheet">
     <link href="{{ asset('bower_components/gentelella/vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{ asset('bower_components/gentelella/vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css')}}" rel="stylesheet">
 @stop
 
 <!-- Script require for this view -->
 @section('viewScripts')
-    <!-- iCheck -->
-    <script src="{{ asset('bower_components/gentelella/vendors/iCheck/icheck.min.js')}}"></script>
     <!-- Datatables -->
     <script src="{{ asset('bower_components/gentelella/vendors/datatables.net/js/jquery.dataTables.min.js')}}"></script>
     <script src="{{ asset('bower_components/gentelella/vendors/datatables.net-bs/js/dataTables.bootstrap.min.js')}}"></script>
-    <script src="{{ asset('bower_components/gentelella/vendors/datatables.net-buttons/js/dataTables.buttons.min.js')}}"></script>
-    <script src="{{ asset('bower_components/gentelella/vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js')}}"></script>
-    <script src="{{ asset('bower_components/gentelella/vendors/datatables.net-buttons/js/buttons.flash.min.js')}}"></script>
-    <script src="{{ asset('bower_components/gentelella/vendors/datatables.net-buttons/js/buttons.html5.min.js')}}"></script>
-    <script src="{{ asset('bower_components/gentelella/vendors/datatables.net-buttons/js/buttons.print.min.js')}}"></script>
-    <script src="{{ asset('bower_components/gentelella/vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js')}}"></script>
-    <script src="{{ asset('bower_components/gentelella/vendors/datatables.net-keytable/js/dataTables.keyTable.min.js')}}"></script>
     <script src="{{ asset('bower_components/gentelella/vendors/datatables.net-responsive/js/dataTables.responsive.min.js')}}"></script>
     <script src="{{ asset('bower_components/gentelella/vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js')}}"></script>
-    <script src="{{ asset('bower_components/gentelella/vendors/datatables.net-scroller/js/dataTables.scroller.min.js')}}"></script>
-    <script src="{{ asset('bower_components/gentelella/vendors/jszip/dist/jszip.min.js')}}"></script>
-    <script src="{{ asset('bower_components/gentelella/vendors/pdfmake/build/pdfmake.min.js')}}"></script>
-    <script src="{{ asset('bower_components/gentelella/vendors/pdfmake/build/vfs_fonts.js')}}"></script>
 
     <script>
       $(document).ready(function() {
-        $('#datatable-responsive').DataTable( {
+        $('#ordersTable-responsive').DataTable( {
             "order": []
         });
       });
     </script>
-
 @stop
 
 
